5c70244e792d1908f055f1a919b19c764e983943
AsunaU2 <kiritou2@outlook.com>
2026-02-12 18:17:07 +0800

    feat: 角色move和jump

--- Rpg Udemy Course/Assets/Player.cs
+++ Rpg Udemy Course/Assets/Player.cs
@@ -1,18 +1,77 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private int facingDir = 1;
+    private bool facingRight = true;
+    private float xInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        CheckInput();
+
+        FlipController();
+        AnimatorControllers();
+
+    }
+
+    private void CheckInput()
+    {
+        xInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight) Flip();
+        else if (rb.velocity.x < 0 && facingRight) Flip();
+
     }
 }
